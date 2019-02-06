0cc62a6637eb49326f21e7b13700e5ca14b5b72a
ryancargill <43156050+ryancargill@users.noreply.github.com>
2019-02-05 20:43:38 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -271,9 +271,9 @@ find "$webPath" -type d -exec chmod g+rwxs {} \;||die
 ############
 echo -e "\n***** Downloading most recent BrewPi codebase... *****"
 cd "$installPath"
-sudo -u brewpi git clone https://github.com/BrewPi/brewpi-script "$installPath"||die
+sudo -u brewpi git clone https://github.com/ryancargill/brewpi-script "$installPath"||die
 cd "$webPath"
-sudo -u www-data git clone https://github.com/BrewPi/brewpi-www "$webPath"||die
+sudo -u www-data git clone https://github.com/ryancargill/brewpi-www "$webPath"||die
 
 ###########
 ### If non-default paths are used, update config files accordingly
